8be2baec33b1ae63c2fc09f01f8ef2e7244288b5
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-07-16 19:09:07 +0545

    Using State (useState)

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
 const Home = () => {
+    const [name, setName] = useState('Diogo');
+    const [num, setNum] =useState(20);
 
-    const handleClick = (e) => {
-        console.log('Hello Guys', e);
+
+   
+    const handleClick = () => {
+
+        setName('Jota');
+        setNum(2020);
+        
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('Hello' + name, e.target);
-    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log('Hello' + name, e.target);
+    // }
 
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is forever {num}</p>
             <button onClick={handleClick}>Click Here</button>
-            <button onClick={(e) => handleClickAgain('Wizard', e)}>Click Here Again</button>
+            {/* <button onClick={(e) => handleClickAgain('Wizard', e)}>Click Here Again</button> */}
 
         </div>
     );
