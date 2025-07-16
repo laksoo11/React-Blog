c98f487defe2af11527b9f59848adc8745bd70a2
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-07-16 18:05:47 +0545

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello Guys', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello' + name, e.target);
+    }
+
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Here</button>
+            <button onClick={(e) => handleClickAgain('Wizard', e)}>Click Here Again</button>
 
         </div>
     );
