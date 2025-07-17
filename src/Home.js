3351a438364eabdd0be47e1753f5e7954e09edde
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-07-17 19:31:30 +0545

    Outputting Lists and added some css

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,43 @@
 import { useState } from "react";
+
 const Home = () => {
-    const [name, setName] = useState('Diogo');
+    const [name, setName] = useState('Diogo'); // initial state
     const [num, setNum] =useState(20);
+
+    const [blogs, setBlogs] = useState([
+        {title: 'My new game', body: 'GPA 5', author: 'PopStars', id:'1'},
+        {title: 'Celebrations', body: 'Party all night', author: 'Frank', id:'2'},
+        {title: 'Top 10 skins 2025', body: 'Best skins to get now', author: 'COC', id:'3'}
+
+
+    ]);
 
 
    
     const handleClick = () => {
 
-        setName('Jota');
+        setName('Jota'); 
         setNum(2020);
         
     }
 
-    // const handleClickAgain = (name, e) => {
-    //     console.log('Hello' + name, e.target);
-    // }
 
-    return (  
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is forever {num}</p>
-            <button onClick={handleClick}>Click Here</button>
-            {/* <button onClick={(e) => handleClickAgain('Wizard', e)}>Click Here Again</button> */}
+  return (  
+    <div className="home">
+        <h2>Homepage</h2>
+        {<p>{name} is forever {num}</p> }
+        <button onClick={handleClick}>Click Here</button>
+        {/* <button onClick={(e) => handleClickAgain('Wizard', e)}>Click Here Again</button>  */}
 
-        </div>
+        {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </div>
+        ))}
+    </div>
     );
-}
+}   
+ 
  
 export default Home;
