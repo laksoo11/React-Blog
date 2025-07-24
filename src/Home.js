98b537c67a869efe8992faaff698ef3848f52a4e
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-07-24 19:06:57 +0545

    Making a Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,73 +1,27 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    // const [name, setName] = useState('Diogo'); // initial state
-    // const [num, setNum] =useState(20);
+    
 
-    const [blogs, setBlogs] = useState(null);
-
-    const [name, setName] = useState('DIGGY');
-
-    const [isPending, setIsPending] = useState(true);
-
-    const [error, setError] = useState(null);
-
-
+  
+   const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
    
 
+    // const [changedstate, setChangedState] = useState('initialstate');
 
+    // const handleState= () => {
+    //     setChangedState('newState');
 
-    const [changedstate, setChangedState] = useState('initialstate');
-
-    const handleState= () => {
-        setChangedState('newState');
-
-    }
-
-   
-    // const handleClick = () => {
-
-    //     setName('Jota'); 
-    //     setNum(2020);
-        
     // }
 
-    useEffect(() => {
-      setTimeout(() => {
-          fetch('http://localhost:8000/blogs')
-            .then(res => {
-                console.log(res);
-
-                if(!res.ok) {
-                    throw Error('could not fetch the data');
-
-                }
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-                
-            })
-      },500);
-
-      
-
-    }, []);
-
-
-
-
+    
   return (  
     <div className="home">
         { error && <div> {error} </div>}
+       
         { isPending && <div>Loading ....</div>}
         {blogs && <BlogList blogs= {blogs} title = "CHAMP20NS"/>}
         {/* <p>{changedstate}</p> */}
@@ -80,13 +34,6 @@ const Home = () => {
             blog.author === 'Frank'
         )} title = "CHAMPEONS"/> */}
         
-
-
-
-
-     
-
-      
     </div>
     );
 }   
