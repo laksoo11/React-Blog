af29752b293109c329da05e055111a1fd7fbb27e
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-07-28 21:57:27 +0545

    Route Parameters

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   const title = 'Welcome to the new blog guys';
@@ -25,6 +26,13 @@ function App() {
 
            <Route path= "/create">
             <Create />
+
+          </Route>
+
+            <Route path= "/blogs/:id">
+            <BlogDetails />
+
+            
 
           </Route>
 
